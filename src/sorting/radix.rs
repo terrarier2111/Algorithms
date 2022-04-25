ea7925f8d6562d8abced18b3300180a5f81b2881
terrarier2111 <58695553+terrarier2111@users.noreply.github.com>
2022-04-25 22:26:34 +0200

    Update radix.rs

--- src/sorting/radix.rs
+++ src/sorting/radix.rs
@@ -1,14 +1,13 @@
-use std::collections::hash_map::Values;
 use std::collections::HashMap;
 
-/// Sorts the given array in ascending order.
-pub(crate) fn sort(src: &[i32]) -> Box<[i32]> {
+pub fn sort(src: &[i32]) -> Box<[i32]> {
     let mut lookup = HashMap::new();
     for i in src {
-        lookup.insert(i,i.to_string());
+        lookup.insert(*i, i.to_string().into_bytes().into_boxed_slice());
     }
-    let mut buckets: [Vec<i32>; 10] = [
-        vec![],
+    let longest = longest(&lookup);
+    let mut buckets = [
+        vec![]/*.into_boxed_slice()*/,
         vec![],
         vec![],
         vec![],
@@ -20,22 +19,28 @@ pub(crate) fn sort(src: &[i32]) -> Box<[i32]> {
         vec![],
     ];
     let mut result = buckets.clone();
-    let longest = longest(lookup.values());
     for x in src.iter() {
         let str_ver = lookup.get(x).unwrap();
-        let mut vec = buckets.get_mut(char_as_num(str_ver.chars().nth(str_ver.len() - 1).unwrap()) as usize).unwrap();
-        vec.push(*x);
+        let mut bucket = buckets.get_mut(char_as_num(str_ver[str_ver.len() - 1] as char) as usize).unwrap();
+        bucket.push(*x);
     }
+
     for i in 1..longest {
-        for bucket in ((0)..10).rev() {
+        for bucket in (0..10).rev() {
             for x in buckets.get(bucket).unwrap().iter() {
                 let str_ver = lookup.get(x).unwrap();
-                let mut vec = result.get_mut(char_as_num(str_ver.chars().nth(str_ver.len() - 1 - i).unwrap_or('0')) as usize).unwrap();
-                vec.push(*x);
+                let idx = str_ver.len() as isize - 1 - i as isize;
+                let elem = if idx >= 0 {
+                    char_as_num(str_ver[idx as usize] as char) as usize
+                } else {
+                    0
+                };
+                let mut bucket = result.get_mut(elem).unwrap();
+                bucket.push(*x);
             }
         }
         for bucket in result.iter() {
-            println!("buck {:?}", bucket);
+            println!("bucket {:?}", bucket);
         }
         let tmp = buckets;
         buckets = result;
@@ -58,9 +63,9 @@ fn char_as_num(input: char) -> u8 {
     (input as u32 - '0' as u32) as u8
 }
 
-fn longest(strs: Values<'_, &i32, String>) -> usize {
+fn longest(strs: &HashMap<i32, Box<[u8]>>) -> usize {
     let mut longest = 0;
-    for str in strs {
+    for str in strs.values() {
         if str.len() > longest {
             longest = str.len();
         }
